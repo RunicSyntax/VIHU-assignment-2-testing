68fdccd4098e1dea63ebc3754b08467f37a8102f
RunicSyntax <107773663+RunicSyntax@users.noreply.github.com>
2025-02-09 20:11:04 +0000

    Realized that date is an actual datatype

--- src/dateUtils.ts
+++ src/dateUtils.ts
@@ -5,18 +5,18 @@ export function getCurrentYear(): number {
   return moment().year();
 }
 
-export function add(date: string, number: number, type: string = DATE_UNIT_TYPES.DAYS): string {
+export function add(date: Date, number: number, type: string = DATE_UNIT_TYPES.DAYS): Date {
   return moment(date).add(number, type).toDate();
 }
 
-export function isWithinRange(date: string, from: string, to: string): boolean {
+export function isWithinRange(date: Date, from: Date, to: string): boolean {
   return moment(date).isBetween(from, to);
 }
 
-export function isDateBefore(date: string, compareDate: string): boolean {
+export function isDateBefore(date: Date, compareDate: Date): boolean {
   return moment(date).isBefore(compareDate);
 }
 
-export function isSameDay(date: string, compareDate: string): boolean {
+export function isSameDay(date: Date, compareDate: Date): boolean {
   return moment(date).isSame(compareDate);
 }
