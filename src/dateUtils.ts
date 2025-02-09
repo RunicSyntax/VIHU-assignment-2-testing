fdf9e5ea477d73ded4446e92231b05a5c43b024e
RunicSyntax <107773663+RunicSyntax@users.noreply.github.com>
2025-02-09 19:51:55 +0000

    Added typing
    I read through quite a bit of documentation and from what I can tell string should work as a type, but I might have to change that depending on how the tests go.

--- src/dateUtils.ts
+++ src/dateUtils.ts
@@ -1,22 +1,22 @@
 import moment from "moment";
 import { DATE_UNIT_TYPES } from "./constants";
 
-export function getCurrentYear() {
+export function getCurrentYear(): number {
   return moment().year();
 }
 
-export function add(date, number, type = DATE_UNIT_TYPES.DAYS) {
+export function add(date: string, number: number, type: string = DATE_UNIT_TYPES.DAYS): string {
   return moment(date).add(number, type).toDate();
 }
 
-export function isWithinRange(date, from, to) {
+export function isWithinRange(date: string, from: string, to: string): boolean {
   return moment(date).isBetween(from, to);
 }
 
-export function isDateBefore(date, compareDate) {
+export function isDateBefore(date: string, compareDate: string): boolean {
   return moment(date).isBefore(compareDate);
 }
 
-export function isSameDay(date, compareDate) {
+export function isSameDay(date: string, compareDate: string): boolean {
   return moment(date).isSame(compareDate);
 }
